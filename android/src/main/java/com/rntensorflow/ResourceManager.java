1a0f9c2e2e44dbdd331e67446e6767a6999cb591
Casey Li <sometimescasey@gmail.com>
2018-11-04 15:27:12 -0500

    loadFromLocal() should look in both 'drawable' and 'raw', update loadFromUrl() for 'createClient() has private access in OkHttpClientProvider'

--- android/src/main/java/com/rntensorflow/ResourceManager.java
+++ android/src/main/java/com/rntensorflow/ResourceManager.java
@@ -3,13 +3,15 @@ package com.rntensorflow;
 import android.content.res.Resources;
 import android.webkit.URLUtil;
 import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.modules.network.OkHttpClientProvider;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
+import android.util.Log;
 
 public class ResourceManager {
 
@@ -35,8 +37,13 @@ public class ResourceManager {
 
     private byte[] loadFromLocal(String resource) {
         try {
-            int identifier = reactContext.getResources().getIdentifier(resource, "drawable", reactContext.getPackageName());
+            // .pb and label .txt go into "raw", .jpg will go into "drawable", so try both
+            int identifier = reactContext.getResources().getIdentifier(resource, "raw", reactContext.getPackageName());
+            if (identifier == 0) {
+                identifier = reactContext.getResources().getIdentifier(resource, "drawable", reactContext.getPackageName());
+            }
             InputStream inputStream = reactContext.getResources().openRawResource(identifier);
+            Log.i("ReactNative", "Successfully returning resource identifier: " + identifier + " for file " + resource);
             return inputStreamToByteArray(inputStream);
         } catch (IOException | Resources.NotFoundException e) {
             try {
@@ -44,10 +51,11 @@ public class ResourceManager {
                 return inputStreamToByteArray(inputStream);
             } catch (IOException e1) {
                 try {
+
                     InputStream inputStream = new FileInputStream(resource);
                     return inputStreamToByteArray(inputStream);
                 } catch (IOException e2) {
-                    throw new IllegalArgumentException("Could not load resource");
+                    throw new IllegalArgumentException("Could not load resource", e2);
                 }
             }
         }
@@ -60,9 +68,11 @@ public class ResourceManager {
     }
 
     private byte[] loadFromUrl(String url) {
+        OkHttpClient client = new OkHttpClient();
+
         try {
             Request request = new Request.Builder().url(url).get().build();
-            Response response = OkHttpClientProvider.createClient().newCall(request).execute();
+            Response response = client.newCall(request).execute();
             return response.body().bytes();
         } catch (IOException e) {
             throw new IllegalStateException("Could not fetch data from url " + url);
